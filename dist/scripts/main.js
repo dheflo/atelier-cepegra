40ea5d6e43cdbd8a232ec78ae7029b93f65012db
dheflo <florentin.dhenau@gmail.com>
2023-12-06 02:33:54 +0100

    Add Nav and hero header

---
+++ dist/scripts/main.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const sections = document.querySelectorAll('.card--active, .card--inactive');
+
+  function cardSwap() {
+    sections.forEach(section => {
+      section.classList.toggle('card--active');
+      section.classList.toggle('card--inactive');
+      section.style.display = (section.classList.contains('card--active')) ? 'flex' : 'none';
+    });
+  }
+
+  sections.forEach(section => {
+    section.addEventListener('click', cardSwap);
+  });
+});
