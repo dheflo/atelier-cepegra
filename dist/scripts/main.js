4c0a89fa5a3704a95e140056f0375fa0176a9a12
dheflo <florentin.dhenau@gmail.com>
2023-12-07 20:36:21 +0100

    Fixed JS script  and cards

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,16 +1,17 @@
 // Card swap style on click
-document.addEventListener('DOMContentLoaded', function() {
-  const sections = document.querySelectorAll('.card--active, .card--inactive');
 
-  function cardSwap() {
-    sections.forEach(section => {
-      section.classList.toggle('card--active');
-      section.classList.toggle('card--inactive');
-      section.style.display = (section.classList.contains('card--active')) ? 'flex' : 'none';
-    });
-  }
+// document.addEventListener('DOMContentLoaded', function() {
+//   const sections = document.querySelectorAll('.card--active, .card--inactive');
 
-  sections.forEach(section => {
-    section.addEventListener('click', cardSwap);
-  });
-});
+//   function cardSwap() {
+//     sections.forEach(section => {
+//       section.classList.toggle('card--active');
+//       section.classList.toggle('card--inactive');
+//       section.style.display = (section.classList.contains('card--active')) ? 'flex' : 'none';
+//     });
+//   }
+
+//   sections.forEach(section => {
+//     section.addEventListener('click', cardSwap);
+//   });
+// });
---
+++ dist/scripts/mainv2.js
@@ -0,0 +1,41 @@
+let cards = document.querySelectorAll('.card');
+
+cards.forEach((card) => {
+  card.addEventListener('click', function() {
+    this.classList.toggle('card-active');
+    
+    let city = this.querySelector('.card__city');
+    if (city) city.classList.toggle('card-active__city');
+
+    let id = this.querySelector('.card__id');
+    if (id) id.classList.toggle('card-active__id');
+
+    let idNames = this.querySelectorAll('.card__id-names');
+    if (idNames) {
+      idNames.forEach((element) => {
+        element.classList.toggle('card-active__id-names');
+      });
+    }
+
+    let thumbnail = this.querySelector('.card__thumbnail');
+    if (thumbnail) thumbnail.classList.toggle('card-active__thumbnail');
+
+    let offline = this.querySelector('.card__offline');
+    if (offline) offline.classList.toggle('card-active__offline');
+
+    let offlineTitle = this.querySelector('.card__offline-title');
+    if (offlineTitle) offlineTitle.classList.toggle('card-active__offline-title');
+
+    let offlineText = this.querySelector('.card__offline-text');
+    if (offlineText) offlineText.classList.toggle('card-active__offline-text');
+
+    let info = this.querySelector('.card__info');
+    if (info) info.classList.toggle('card-active__info');
+
+    let infoTime = this.querySelector('.card__info-time');
+    if (infoTime) infoTime.classList.toggle('card-active__info-time');
+
+    let infoCTA = this.querySelector('.card__info-cta');
+    if (infoCTA) infoCTA.classList.toggle('card-active__info-cta');
+  });
+});
